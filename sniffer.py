a88076a78d2439c7d0a94b24678cb04949e9ab30
Ian O'Connell <ianoconnell01@gmail.com>
2018-10-16 01:41:24 +0100

    Sniffer

--- sniffer.py
+++ sniffer.py
@@ -8,3 +8,18 @@ print(type(capture))
 print(len(capture))
 print(capture)
 
+# Loops through pcap lines
+
+for i in capture:
+
+    ethernet_frame = capture[i]
+    ip_packet = ethernet_frame.payload
+    segment = ip_packet.payload
+    data = segment.payload
+
+    print("Packet #", i)
+    print(ethernet_frame.summary())
+    print(ip_packet.summary())
+    print(segment.summary())
+    print(data.summary())
+    i += 1
